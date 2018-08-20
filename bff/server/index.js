084d9d3992bf8b224906e75b497bf54efb88b3d7
--global <--global>
2018-08-19 22:46:25 -0400

    added fe

--- bff/server/index.js
+++ bff/server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
+const cors = require('cors')
 const db = mongoose.connection;
 
 const serverAddress = "mongodb://127.0.0.1:27017/login";
@@ -12,6 +13,8 @@ db.on('error', console.error.bind(console, 'Mongoose Db Connection Error'));
 db.once('open', () => {
     console.log('Db Connected on: ' + serverAddress);
 
+    //Adding CORS for local testing, should be removed if in prod
+    app.use(cors());
     app.use('/', require('./routes'));
     app.listen(3000, () => console.log('Server is now running at port 3000'));
 });
