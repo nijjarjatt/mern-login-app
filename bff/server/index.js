7639c572416ca8f9838debbb44cf9cc2eceeccf4
Talwinder Singh <nijjar.login@gmail.com>
2019-09-17 15:54:21 -0400

    added yarn removed npm

--- bff/server/index.js
+++ bff/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const router = express.Router();
-const cors = require('cors')
+const cors = require('cors');
 const db = mongoose.connection;
 
 const serverAddress = "mongodb://127.0.0.1:27017/login";
